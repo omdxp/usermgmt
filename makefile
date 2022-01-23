fc7e21d62c4fa43060197bca51d6d94cdff7579d
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-01-23 01:29:07 +0100

    defining client and create new users

--- makefile
+++ makefile
@@ -1,2 +1,8 @@
 genpb:
 	protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative usermgmt/usermgmt.proto
+
+run_server:
+	go run usermgmt_server/usermgmt_server.go
+
+run_client:
+	go run usermgmt_client/usermgmt_client.go
--- usermgmt_client/usermgmt_client.go
+++ usermgmt_client/usermgmt_client.go
@@ -1,1 +1,40 @@
 package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	pb "github.com/Omar-Belghaouti/usermgmt/usermgmt"
+	"google.golang.org/grpc"
+)
+
+const (
+	address = "localhost:50051"
+)
+
+func main() {
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	c := pb.NewUserManagementClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	var new_users = make(map[string]int32)
+	new_users["Omar"] = 23
+	new_users["Yasser"] = 8
+
+	for name, age := range new_users {
+		r, err := c.CreateNewUser(ctx, &pb.NewUser{Name: name, Age: age})
+		if err != nil {
+			log.Fatalf("could not greet: %v", err)
+		}
+		log.Printf("User created: %v, %v, %v", r.GetName(), r.GetAge(), r.GetId())
+	}
+
+}
